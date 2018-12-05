a3055cb6dfdf2c55fa3269668620e16db4f0f224
Piotr Lasota <lasota.piotr@live.com>
2018-12-05 15:13:30 +0100

    initial commit

---
+++ Core/Entities/ResultType.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Entities
+{
+    public class ResultType
+    {
+        public Environment Environment { get; }
+
+        public SearchCriteria searchCriteria { get; }
+
+        public ResultType(Environment environment, SearchCriteria searchCriteria)
+        {
+            this.Environment = environment;
+            this.searchCriteria = searchCriteria;
+        }
+    }
+}
---
+++ Core/Entities/SearchCriteria.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Core.Entities
+{
+    public class SearchCriteria
+    {
+        private readonly IList<Regex> regexes;
+
+        public SearchCriteria(IList<Regex> regexes)
+        {
+            this.regexes = regexes;
+        }
+    }
+}
